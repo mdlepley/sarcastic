7261d83c1c599864193ba677bd9e288296e21302
Matt Lepley <mdlepley@gmail.com>
2014-01-31 19:35:17 -0600

    First files
    Some of the basic functions

---
+++ sarcastic.js
@@ -0,0 +1,26 @@
+// This is sarcastic.js,
+// a JavaScript library
+// designed for human readability.
+// Talk normally through code.
+
+
+var isScrewed = true;
+
+String.prototype.yell = function() {
+	return this.toUpperCase();
+}
+
+String.prototype.whisper = function() {
+	return this.toLowerCase();
+}
+
+String.prototype.wowYourFriends = function() {
+	// blinking text
+	// use timeouts to control element's display property
+}
+
+function blinkingText(obj) {
+	// accept a jQuery object as param (if none, use document)
+	// get text in param
+	// for each text, do .wowYourFriends()
+}
